f982fcc81dd51b087d15fd0e7d341c43aede7d8d
Andrey-Peregudov <anmrya12@gmail.com>
2025-03-16 18:23:10 +0300

    feature commit

--- app/converter/converter_file.py
+++ app/converter/converter_file.py
@@ -1,21 +1,18 @@
-from fastapi import Form, Request, APIRouter
+from fastapi import Request, APIRouter, UploadFile
 from fastapi.responses import HTMLResponse
-from template_metod import templates
+from ..template_metod import templates
 
 router = APIRouter()
 
 templates = templates
 
 @router.get("/converter_file", tags=["Конвертер файлов"], response_class=HTMLResponse)
-def get_degree_form(request: Request):
+async def get_degree_form(request: Request):
     return templates.TemplateResponse("converter_file.html", {"request": request})
 
 @router.get('/converter_file', response_class=HTMLResponse, summary="Конвертация файлов", tags=["Конвертёр файлов"])
 @router.post('/converter_file', response_class=HTMLResponse, summary="Конвертация файлов", tags=["Конвертёр файлов"])
-async def convert_dec_degree(request: Request,
-                           degree_dec : float = Form(le=360)):
-    try:
-        degree_dec
-        return templates.TemplateResponse("converter_file.html", {"request": request, "degree_dec": degree_dec})
-    except ValueError:
-        return templates.TemplateResponse("converter_file.html", {"request": request, "error": "Ошибка ввода. Введите значения заново"})
+async def create_upload_file(request: Request, file: UploadFile):
+    filename = file.size
+    print(filename)
+    return templates.TemplateResponse("converter_file.html", {"request": request, "filename": filename})
--- app/main.py
+++ app/main.py
@@ -34,10 +34,12 @@ templates = templates
 def root():
     return FileResponse("templates/base.html")
 
+#Шаблон банера ошибки 404
 @app.exception_handler(404)
 async def custom_404_handler(request, __):
     return templates.TemplateResponse("404.html", {"request": request})
 
+#Шаблон банера ошибки 422
 @app.exception_handler(422)
 async def custom_422_handler(request, __):
     return templates.TemplateResponse("422.html", {"request": request})
