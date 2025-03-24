7fbe42566b921c400f09853dd6b2c10affc58a0a
Andrey-Peregudov <anmrya12@gmail.com>
2025-03-24 21:22:56 +0300

    24/03/2025 21:22

--- app/converter/converter_file.py
+++ app/converter/converter_file.py
@@ -1,4 +1,6 @@
-from fastapi import Request, APIRouter, UploadFile
+from typing import Optional
+from PIL import Image
+from fastapi import Request, APIRouter, UploadFile, Form
 from fastapi.responses import HTMLResponse
 from ..template_metod import templates
 
@@ -11,7 +13,27 @@ def get_file_convert_form(request: Request):
 
 @router.get('/converter_file', response_class=HTMLResponse, summary="Конвертация файлов", tags=["Конвертёр файлов"])
 @router.post('/converter_file', response_class=HTMLResponse, summary="Конвертация файлов", tags=["Конвертёр файлов"])
-def create_upload_file(request: Request, file: UploadFile):
-    filename = file.size
-    print(filename)
-    return templates.TemplateResponse("converter_file.html", {"request": request})
+async def create_upload_file(request: Request,
+                       file: UploadFile,
+                       conversion_type: str = Form(...)):
+    try:
+        filename = file.filename
+        contents = await file.read()
+        image = Image.open(io.BytesIO(contents))
+        if conversion_type == "jpeg_to_tif":
+            converted_filename = filename + ".tif"
+        elif conversion_type == "tif_to_jpeg":
+            converted_filename = filename + ".jpg"
+        elif conversion_type == "pdf_to_jpeg":
+            converted_filename = filename + ".jpg"
+        else:
+            raise HTTPException(status_code=400, detail="Invalid conversion type")
+        return templates.TemplateResponse("converter_file.html", {"request": request, "filename": filename, "converted_filename": converted_filename})
+
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail=str(e))
+
+# from starlette.background import BackgroundTask
+# # ...def cleanup():
+#     os.remove(temp_file)return FileResponse(temp_file, background=BackgroundTask(cleanup),)
--- app/to_do_list/to_do_list.py
+++ app/to_do_list/to_do_list.py
@@ -1,5 +1,6 @@
-from turtledemo.sorting_animate import partition
-from fastapi import Form, Request, APIRouter
+from fastapi import Form, Request, APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from datetime import datetime
 from fastapi.responses import HTMLResponse
 from ..template_metod import templates
 from jose import jwt
@@ -18,7 +19,7 @@ def to_do_page(request: Request):
 @router.post("/to_do_list", response_class=HTMLResponse, summary="Список дел", tags=["Список дел"])
 async def to_do(request: Request,
                 db: AsyncSession = Depends(get_session),
-                to_do: str = Form(str)):
+                to_do: str = Form(...)):
     errors = []
     try:
         token = request.cookies.get("access_token")
@@ -26,12 +27,18 @@ async def to_do(request: Request,
             errors.append("Авторизуйтесь")
             return templates.TemplateResponse("to_do_list.html", {"request": request, "errors":errors})
         else:
-            shema,_, param = token,partition(" ")
+            shema,_, param = token.partition(" ")
             payload = jwt.decode(param, key, algorithm)
             email = payload.get("sub")
             user = db.query(UserDB).filter(UserDB.email==email).first()
             if user is None:
                 errors.append("Вы не прошли аутентификацию")
                 return templates.TemplateResponse("to_do_list.html", {"request": request, "error": errors})
+            else:
+                item = CaseDB(case_text=to_do, data_time=datetime.now(), status=False, autor_case=user.id)
+                db.add(item)
+                await db.commit()
+                await db.refresh(item)
+                return templates.TemplateResponse("to_do_list.html", {"request": request, "message": "Дело добавлено"})
     except ValueError:
         return templates.TemplateResponse("to_do_list.html", {"request": request, "error": "Ошибка ввода. Введите значения заново"})
